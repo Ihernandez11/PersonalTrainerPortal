3aa99ae022bc2b9cc249378436af33bbd8cb1d05
Ihernandez11 <35441196+Ihernandez11@users.noreply.github.com>
2018-06-06 21:11:53 -0500

    Add project files.

---
+++ PersonalTrainerPortal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PersonalTrainerPortal.Startup))]
+namespace PersonalTrainerPortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
